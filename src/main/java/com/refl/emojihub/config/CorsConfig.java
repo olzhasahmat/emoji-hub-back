a72011bf692d6da252f506902bf80f9afb5325f6
OAkhmatullin <oakhmatullin@beeline.kz>
2025-12-28 21:49:21 +0500

    Fix cors

--- src/main/java/com/refl/emojihub/config/CorsConfig.java
+++ src/main/java/com/refl/emojihub/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://emoji-hub-vercel.vercel.app")
+                .allowedOrigins("https://*.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
